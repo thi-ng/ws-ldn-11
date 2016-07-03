70f4ea65edc3f3e5b261474ec202e3880c9dc035
Karsten Schmidt <k@postspectacular.com>
2016-07-03 14:39:47 +0100

    cleanup ex05b.core ns

--- day2/ex05b/src/ex05b/core.cljs
+++ day2/ex05b/src/ex05b/core.cljs
@@ -1,20 +1,18 @@
 (ns ex05b.core
   (:require-macros
-   [reagent.ratom :refer [reaction]]
-   [cljs-log.core :refer [debug info warn]])
+   [reagent.ratom :refer [reaction]])
   (:require
    [thi.ng.math.core :as m :refer [PI HALF_PI TWO_PI]]
    [thi.ng.color.core :as col]
    [thi.ng.geom.core :as g]
-   [thi.ng.geom.vector :as v :refer [vec2 vec3]]
-   [thi.ng.geom.matrix :as mat :refer [M44]]
+   [thi.ng.geom.vector :as v]
+   [thi.ng.geom.matrix :as mat]
    [thi.ng.geom.gl.core :as gl]
    [thi.ng.geom.gl.webgl.constants :as glc]
    [thi.ng.geom.gl.webgl.animator :as anim]
    [thi.ng.geom.gl.shaders :as sh]
    [thi.ng.geom.gl.glmesh :as glm]
    [thi.ng.geom.gl.shaders.phong :as phong]
-   [thi.ng.glsl.core :as glsl :include-macros true]
    [reagent.core :as reagent]))
 
 (defonce app (reagent/atom {}))
@@ -52,10 +50,10 @@
   (let [gl       (gl/gl-context (reagent/dom-node this))
         vport    (gl/get-viewport-rect gl)
         shader   (sh/make-shader-from-spec gl phong/shader-spec)
-        uniforms {:model         M44
+        uniforms {:model         mat/M44
                   :proj          (mat/perspective 60 vport 0.1 10)
                   :view          (mat/look-at (v/vec3 0 0 1.25) (v/vec3) v/V3Y)
-                  :lightPos      (vec3 0.1 0 1)
+                  :lightPos      (v/vec3 0.1 0 1)
                   :ambientCol    0x000011
                   :diffuseCol    0x0033ff
                   :specularCol   0xffffff
@@ -78,7 +76,7 @@
           (gl/clear-color-and-depth-buffer (col/rgba 0.3 0.3 0.3) 1)
           (gl/draw-with-shader
            (-> model
-               (update-in [:uniforms :model] #(m/* (g/rotate-y M44 t) %)))))))
+               (update-in [:uniforms :model] #(m/* (g/rotate-y mat/M44 t) %)))))))
     (:active (reagent/state this))))
 
 (defn gl-component
@@ -91,7 +89,6 @@
       (anim/animate ((:loop props) this)))
     :component-will-unmount
     (fn [this]
-      (debug "unmount GL")
       (reagent/set-state this {:active false}))
     :reagent-render
     (fn [_]
@@ -115,21 +112,10 @@
   []
   [:div
    [:div [mesh-selecta]]
-   [gl-component
-    {:init          init-gl
-     :loop          update-gl
-     ;;:on-mouse-move (fn [e] (game/update-player-pos! (.-clientX e)))
-     ;;:on-mouse-down (fn [e] (game/player-max-speed!))
-     ;;:on-mouse-up   (fn [e] (game/player-normal-speed!))
-     ;;:on-touch-move (fn [e]
-     ;;                 (game/update-player-pos! (.-clientX (aget (.-touches e) 0)))
-     ;;                 (game/player-max-speed!))
-     ;;:on-touch-end  (fn [e] (game/player-normal-speed!))
-     }]])
+   [gl-component {:init init-gl :loop update-gl}]])
 
 (defn main
   []
-  (.initializeTouchEvents js/React)
   (let [worker (js/Worker. "js/meshworker.js")]
     (set! (.-onmessage worker) receive-mesh!)
     (swap! app assoc :worker worker)
